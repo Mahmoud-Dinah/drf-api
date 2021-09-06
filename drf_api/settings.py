2788828687053cd1b71787a71b27eb5674b4c9ff
mahmoud-dinah <mahmood.dinah2@gmail.com>
2021-09-06 08:22:11 +0300

    finish lab req.

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES' : [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
